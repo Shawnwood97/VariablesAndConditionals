d302fba6843cacf1e59cc769b3c4d6047c54a860
Shawn Wood <Shawnwood97@gmail.com>
2021-03-18 20:38:02 -0600

    finished W9B with bonus and comments

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-var age = 17;
-var isSubscribed = true;
+// Array of ages
+var age = [15, 18, 30, 5];
 
-if (age < 18 && isSubscribed === false) {
-  console.log("the user is younger than 18 and not subscribed");
-} else if (age >= 18 && isSubscribed === false) {
-  console.log("the user is 18 and older and not subscribed");
-} else if (age < 18 && isSubscribed === true) {
-  console.log("the user is younger than 18 and is subscribed");
-} else if (age >= 18 && isSubscribed === true) {
-  console.log("the user is 18 or older and is subscribed");
-} else {
-  console.log("something is very very very wronggggggg?!?!?!?!?");
+// Array of subscribed status
+var isSubscribed = [false, false, true, true];
+
+// Getting length of items in the Array to make the loop dynamic
+var arrayLength = age.length;
+
+// For loop that adds +1 to variable "i" every time it loops.
+// Each loop, the counter is applied to the end of the variabless to indicate which item it is running.
+for (var i = 0; i < arrayLength; i++) {
+  if (age[i] < 18 && !isSubscribed[i]) {
+    console.log("the user is younger than 18 and not subscribed");
+  } else if (age[i] >= 18 && !isSubscribed[i]) {
+    console.log("the user is 18 and older and not subscribed");
+  } else if (age[i] < 18 && isSubscribed[i]) {
+    console.log("the user is younger than 18 and is subscribed");
+  } else if (age[i] >= 18 && isSubscribed[i]) {
+    console.log("the user is 18 or older and is subscribed");
+  } else {
+    console.log("something is very very very wronggggggg?!?!?!?!?");
+  }
 }
